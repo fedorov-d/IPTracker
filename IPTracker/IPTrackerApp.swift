1b09492fd868271a45983287fc82da7f6da28666
Dmitry Fedorov <fedorov@duck.com>
2024-01-13 15:31:32 +0400

    Initial Commit

---
+++ IPTracker/IPTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPTrackerApp.swift
+//  IPTracker
+//
+//  Created by Dmitry Fedorov on 13.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct IPTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
